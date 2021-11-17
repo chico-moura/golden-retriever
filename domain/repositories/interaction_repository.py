652921476c300116ec4764b74e967b6e02b40b8e
chico <chico.zimcolor@gmail.com>
2021-11-17 09:41:05 -0300

    repository method is now abstract

--- domain/repositories/interaction_repository.py
+++ domain/repositories/interaction_repository.py
@@ -1,4 +1,4 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 from typing import List
 
 from domain.entities import Interaction
@@ -6,5 +6,6 @@ from domain.value_objects.user_id import UserID
 
 
 class InteractionRepository(ABC):
+    @abstractmethod
     def fetch_last_interactions(self, user_id: UserID, amount: int) -> List[Interaction]:
         pass
