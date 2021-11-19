1002a294ed37ea44568453d739346114ae348cfd
chico <chico.zimcolor@gmail.com>
2021-11-18 23:08:42 -0300

    gbrennon review

--- infrastructure/repositories/instagram/instagram_api_url.py
+++ infrastructure/repositories/instagram/instagram_api_url.py
@@ -1,6 +1,8 @@
 from domain.value_objects import UserAccount
 
 
+# FIXME: InstagramAPIClient (put requests inside here and make a method .get)
+# FIXME: maybe user account is a dependency?
 class InstagramAPIURL:
     __BASE_URL: str = f'https://graph.instagram.com/v12.0/me'
 
@@ -12,3 +14,6 @@ class InstagramAPIURL:
     @staticmethod
     def __format_access_token(token: str) -> str:
         return f'&access_token={token}'
+
+    def __init__(self, url):
+        self.__BASE_URL = url
